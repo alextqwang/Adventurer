de34d581e2815c61de3439b62c550cb8386e235d
alextqwang <alextqwang@gmail.com>
2026-08-25 19:02:29 -0400

    score created in scoring file, gives an actual score based on urgency and importance

--- adventurer/scoring.py
+++ adventurer/scoring.py
@@ -1,6 +1,8 @@
 from datetime import date
 from typing import Optional
 
+from adventurer.task import Task
+
 # CONSTANTS
 URGENCY_ANCHORS = [(0, 1.0), (2, 0.9), (3, 0.7), (7, 0.5), (8, 0.25), (14, 0.1), (30, 0.0)]
 
@@ -13,9 +15,11 @@ def urgency(deadline: Optional[date]) -> float:
     elif days_left <= 0:
         return 1.0
     else:
-        return find_urgency(days_left)
+        return _find_urgency(days_left)
 
-def find_urgency(days: int) -> float:
+def _find_urgency(days: int) -> float:
+    """ Assume 0 < days < 30, _find_urgency is a helper to calculate the correct
+    urgency value using linear interpolation """
     i = 0
     while days > URGENCY_ANCHORS[i][0]:
         i += 1
@@ -24,5 +28,8 @@ def find_urgency(days: int) -> float:
     urgency_value = (anchor1[1] - anchor2[1]) / (anchor2[0] - anchor1[0]) * (anchor2[0] - days) + anchor2[1]
     return urgency_value
 
+def score(task: Task) -> float:
+    task_urgency = urgency(task.deadline)
+    return task.importance * (0.5 + task_urgency)
 
 
