549eb506a19f0ceb22e5abf8e12c5aa484142b46
alextqwang <alextqwang@gmail.com>
2026-08-31 16:37:15 -0400

    Fixing up the streamlit app with buttons and task display.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from adventurer.parser import parse
 from adventurer.scoring import sort_tasks
+from datetime import date, timedelta
 
 if "tasks" not in st.session_state:
     st.session_state["tasks"] = []
@@ -14,13 +15,22 @@ if st.button("Add Tasks") and dump:
     st.session_state["tasks"].extend(tasks)
 
 st.session_state["tasks"] = sort_tasks(st.session_state["tasks"])
+current_session = [task for task in st.session_state["tasks"] if task.snoozed_until is None or task.snoozed_until <= date.today()]
 
-i = 0
-st.write(st.session_state["tasks"][i])
+if current_session:
+    current_task = current_session[0]
+    st.subheader(current_task.text)
+    if current_task.deadline is not None:
+        st.write(f"Deadline: {current_task.deadline}")
+    else:
+        st.write("No Deadline")
+    if st.button("Done!"):
+        st.session_state["tasks"].remove(current_task)
+        st.rerun()
+    if st.button("Not Now..."):
+        current_task.snoozed_until = date.today() + timedelta(days = 1)
+        st.rerun()
+else:
+    st.write("No more tasks!")
 
-if st.button("Done!"):
-    st.session_state["tasks"] = st.session_state["tasks"][1:]
 
-if st.button("Not Now!"):
-    i += 1
-    st.write(st.session_state["tasks"][i])
